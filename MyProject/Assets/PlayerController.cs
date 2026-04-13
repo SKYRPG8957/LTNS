8848b93ae42461f1fdfa3d1fb4bd33986dc952c1
SKYRPG8957 <t01038679520@gmail.com>
2026-04-13 21:01:04 +0900

    update

---
+++ MyProject/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+
+{
+    private Vector2 moveInput;
+
+    public float moveSpeed = 15f;
+    public float jumpForce = 100f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+    }
+}
+
